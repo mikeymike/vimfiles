6197ceb5353eb266ca8ca330472f0313ba4adb5c
mikeymike <mikeymike.mw@gmail.com>
2020-12-24 22:27:22 +0000

    Disable VIM changes cause ARM

--- install.sh
+++ install.sh
@@ -2,4 +2,4 @@
 git clone --recursive https://github.com/marcel-dias/vimfiles.git ~/.vimfiles
 ln -s ~/.vimfiles/vim.symlink ~/.vim
 ln -s ~/.vimfiles/vimrc.symlink ~/.vimrc
-vim +PluginInstall +qall
+#vim +PluginInstall +qall

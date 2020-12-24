f3f6dcdb675df431f926c77061c1c7485c932133
mikeymike <mikeymike.mw@gmail.com>
2020-12-24 22:48:08 +0000

    Use forked repo

--- install.sh
+++ install.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
-git clone --recursive https://github.com/imkeymike/vimfiles.git ~/.vimfiles
+git clone --recursive https://github.com/mikeymike/vimfiles.git ~/.vimfiles
 ln -s ~/.vimfiles/vim.symlink ~/.vim
 ln -s ~/.vimfiles/vimrc.symlink ~/.vimrc
 vim +PluginInstall +qall

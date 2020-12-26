a39502f8cec559d57a675b41a197a2ab5ba53d70
mikeymike <mikeymike.mw@gmail.com>
2020-12-26 20:29:42 +0000

    Try quiter install of plugins

--- install.sh
+++ install.sh
@@ -7,4 +7,5 @@ fi
 git clone --recursive https://github.com/mikeymike/vimfiles.git ~/.vimfiles
 ln -sf ~/.vimfiles/vim.symlink ~/.vim
 ln -sf ~/.vimfiles/vimrc.symlink ~/.vimrc
-vim +PluginInstall +qall
+
+vim +PluginInstall +qall >/tmp/vim-plugins.log 2>&1 || true
